1c0f7c2b52ac25758ff2df34e3a0c3dd0ad0bc90
baolong23 <huynhbaolong235@gmail.com>
2025-07-18 23:38:59 +0700

    Update customer section swipe animation

--- tt-rice-app/src/app/about/section/CustomerSection.tsx
+++ tt-rice-app/src/app/about/section/CustomerSection.tsx
@@ -3,8 +3,8 @@
 import Image from "next/image";
 import React, { use, useState, useEffect } from "react";
 import type { Testimonial } from "~/app/types/Types"
-import { useMediaQuery } from "~/app/hooks/useMediaQuery"
-;import { motion, AnimatePresence } from 'framer-motion';
+import { useMediaQuery } from "~/app/hooks/useMediaQuery";
+import { motion, AnimatePresence , type Variants } from 'framer-motion';
 
 
 const testimonials: Testimonial[] = [
@@ -34,67 +34,10 @@ type MobileCardProps = {
   testimonial: Testimonial;
   setCurrentIndex: React.Dispatch<React.SetStateAction<number>>;
   currentIndex: number;
+  direction: number;
+  variants: Variants;
 };
-function MobileCard({ testimonial, setCurrentIndex, currentIndex }: MobileCardProps){
-  return(
-    <section className="pt-20 pb-5 bg-[#FBFFF2]">
-        <div className="max-w-4xl mx-auto px-5 text-center flex flex-col items-center leading-[1]">
-          <h2 className="text-[42px] font-bold mb-2 font-alegreya-sans" style={{ color: "#0A5B89" }}>
-            Hạt gạo Tư Trúc qua câu chuyện của mỗi người dùng
-          </h2>
-          <p className="text-[28px] mb-16 font-alegreya-sans text-blue-normal">
-            Khách hàng nói gì về Tư Trúc?
-          </p>
-          <div className="flex-shrink-0 md:flex-[0.25]" >
-            <Image src="/img_.svg" alt="Quote" width={56} height={39} />
-          </div>
-          <div className="py-6">
-            <p className="max-w-[566px] text-[14px] font-fz-poppins text-gray-light leading-relaxed flex-1" style={{color:"#667085"}}>
-              {testimonial.content}
-            </p>
-          </div>
-          <div className="flex flex-row items-center py-6 gap-4 md:flex-[1]">
-            <Image
-              src={testimonial.avatar}
-              alt={testimonial.author}
-              width={56}
-              height={56}
-              className="rounded-full"
-            />
-            <div className="text-left">
-              <h4 className="text-[20px] font-bold font-alegreya-sans text-blue-dark mb-1" style={{color:"#0A5B89"}}>
-                {testimonial.author}
-              </h4>
-              <p className="text-gray-muted font-fz-poppins "style={{color:"#5C6578"}}>
-                {testimonial.position}
-              </p>
-            </div>
-          </div>
-          <div className="flex gap-2 mt-2 sm:mt-4">
-              {testimonials.map((item, index) => (
-                <button
-                  key={index}
-                  onClick={() => setCurrentIndex(index)}
-                  className="rounded-full overflow-hidden transition duration-300"
-                >
-                  <Image
-                    src={
-                      index === currentIndex
-                        ? '/Pagination_current.svg'
-                        : '/Pagination.svg'
-                    }
-                    alt={item.author}
-                    width={18}
-                    height={8}
-                    className="object-cover"
-                  />
-                </button>
-              ))}
-            </div>
-        </div>
-    </section>
-  );
-}
+
 export function CustomerSection() {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const testimonial = testimonials[currentIndex];
@@ -136,6 +79,8 @@ export function CustomerSection() {
         testimonial={testimonial}
         setCurrentIndex={setCurrentIndex}
         currentIndex={currentIndex}
+        direction={direction}
+        variants={variants}
          /> :
     <section className="pt-20 pb-5 bg-[#FBFFF2]">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 text-center flex flex-col items-center">
@@ -207,6 +152,80 @@ export function CustomerSection() {
                   </button>
                 ))}
               </div>
+            </motion.div>
+            </AnimatePresence>
+        </div>
+    </section>
+  );
+}
+
+function MobileCard({ testimonial, setCurrentIndex, currentIndex ,direction, variants}: MobileCardProps){
+  return(
+    <section className="pt-20 pb-5 bg-[#FBFFF2]">
+        <div className="max-w-4xl mx-auto px-5 text-center flex flex-col items-center leading-[1]">
+          <h2 className="text-[42px] font-bold mb-2 font-alegreya-sans" style={{ color: "#0A5B89" }}>
+            Hạt gạo Tư Trúc qua câu chuyện của mỗi người dùng
+          </h2>
+          <p className="text-[28px] mb-16 font-alegreya-sans text-blue-normal">
+            Khách hàng nói gì về Tư Trúc?
+          </p>
+          <AnimatePresence custom={direction} mode="wait">
+          <motion.div
+            key={testimonials[currentIndex]?.id}
+            custom={direction}
+            variants={variants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{ duration: 0.5 }}
+            className="w-full flex flex-col items-center "
+          >
+          <div className="flex-shrink-0 md:flex-[0.25]" >
+            <Image src="/img_.svg" alt="Quote" width={56} height={39} />
+          </div>
+          <div className="py-6">
+            <p className="max-w-[566px] text-[14px] font-fz-poppins text-gray-light leading-relaxed flex-1" style={{color:"#667085"}}>
+              {testimonial.content}
+            </p>
+          </div>
+          <div className="flex flex-row items-center py-6 gap-4 md:flex-[1]">
+            <Image
+              src={testimonial.avatar}
+              alt={testimonial.author}
+              width={56}
+              height={56}
+              className="rounded-full"
+            />
+            <div className="text-left">
+              <h4 className="text-[20px] font-bold font-alegreya-sans text-blue-dark mb-1" style={{color:"#0A5B89"}}>
+                {testimonial.author}
+              </h4>
+              <p className="text-gray-muted font-fz-poppins "style={{color:"#5C6578"}}>
+                {testimonial.position}
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-2 mt-2 sm:mt-4">
+              {testimonials.map((item, index) => (
+                <button
+                  key={index}
+                  onClick={() => setCurrentIndex(index)}
+                  className="rounded-full overflow-hidden transition duration-300"
+                >
+                  <Image
+                    src={
+                      index === currentIndex
+                        ? '/Pagination_current.svg'
+                        : '/Pagination.svg'
+                    }
+                    alt={item.author}
+                    width={18}
+                    height={8}
+                    className="object-cover"
+                  />
+                </button>
+              ))}
+            </div>
             </motion.div>
             </AnimatePresence>
         </div>
