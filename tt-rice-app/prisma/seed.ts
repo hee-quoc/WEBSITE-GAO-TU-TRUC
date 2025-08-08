1d670badab672b642ba45022957e8b5c610e625f
hee-quoc <bebeiu21149@student.hcmiu.edu.vn>
2025-08-08 20:43:53 +0700

    update seed n schema

--- tt-rice-app/prisma/seed.ts
+++ tt-rice-app/prisma/seed.ts
@@ -58,60 +58,27 @@ async function main() {
   {
     name: 'Gạo ST25 Lúa Tôm',
     description: `
-      <div class="content mx-auto w-full ">
-        <p class="text-gray-800 mb-4">
-          Gạo sạch Tư Trúc - Gạo ST25 nổi tiếng với hạt gạo thon dài, trắng trong. Khi nấu, cơm dẻo mềm, có mùi thơm đặc trưng của lá dứa và cốm non, vị ngọt đậm đà. Đây là giống gạo được ưa chuộng bởi hương vị thơm ngon và giá trị dinh dưỡng cao, thích hợp cho mọi bữa ăn gia đình.
+       <div class="content mx-auto w-full  font-fz-poppins">
+        <p class=" mb-4">
+          “Từ vùng đất lúa - tôm giàu dưỡng chất, Tư Trúc tạo nên những hạt gạo sạch, dẻo thơm, được kết tinh từ mùa vụ duy nhất trong năm, hấp thụ trọn vẹn tinh hoa đất trời.”
         </p>
-        <div class="text-center  mb-4">
-          Hàm lượng dinh dưỡng trong 01kg
-        </div>
-        <div class="flex justify-between mb-4">
-          <table class="w-2/3 border-collapse border border">
-            <tbody>
-              <tr class="border border">
-                <td class="border border p-2">Đạm > 60g</td>
-              </tr>
-              <tr class="border border">
-                <td class="border border p-2">Tinh bột (Cacbonhydrat) > 720g</td>
-              </tr>
-              <tr class="border border">
-                <td class="border border p-2">Chất xơ (Fiber) > 3g</td>
-              </tr>
-            </tbody>
-          </table>
-          <table class="w-1/3 border-collapse border border">
-            <tbody>
-              <tr class="border border">
-                <td class="border border p-2">Chất béo (Lipid) < 2g</td>
-              </tr>
-              <tr class="border border">
-                <td class="border border p-2">Vitamin B1 < 100 μg</td>
-              </tr>
-              <tr class="border border">
-                <td class="border border p-2">Cholesterol = 0 g</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="text-center mb-4">
-          Chỉ tiêu chất lượng
-        </div>
-        <div class="flex justify-between">
-          <table class="w-2/3 border-collapse border border">
-            <tbody>
-              <tr class="border border">
-                <td class="border border p-2">Độ ẩm < 14%</td>
-              </tr>
-            </tbody>
-          </table>
-          <table class="w-1/3 border-collapse border border">
-            <tbody>
-              <tr class="border border">
-                <td class="border border p-2">Tấm < 5%</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <ul class="pl-5 space-y-2">
+          <li>
+            <p>
+              Gạo Lúa Tôm ST25 được canh tác theo mô hình luân canh lúa - tôm tự nhiên, hạn chế tối đa hóa chất, giúp đất lành, nước sạch, giữ cho hạt gạo có hương vị nguyên bản và thuần khiết.
+            </p>
+          </li>
+          <li>
+            <p>
+              Tận dụng thổ nhưỡng giàu khoáng tại vùng trồng Bà Rịa - Vũng Tàu, Tư Trúc gieo trồng và cho ra những hạt gạo trắng ngà, thon dài, đều tăm tắp, không lẫn tạp chất, khi chạm vào thấy hạt gạo mịn, chắc mẩy.
+            </p>
+          </li>
+          <li>
+            <p>
+              Khi nấu chín, cơm thoảng hương thơm dịu như lá dứa quyện cốm non, nhẹ nhàng mà vẫn đủ lan tỏa khắp gian bếp. Hạt cơm dẻo mềm, tơi đều, vừa chạm đầu lưỡi đã thấy vị ngọt thanh, hậu vị đậm dần theo từng lần nhai. Dù dùng khi còn nóng hay đã nguội, cơm vẫn thơm ngon dễ ăn, phù hợp không chỉ cho mọi bữa cơm gia đình, mà còn cho các bếp ăn cao cấp lẫn thị trường xuất khẩu.
+            </p>
+          </li>
+        </ul>
       </div>
     `,
     imageFilename: 'img_eat_rice.png',
@@ -124,60 +91,27 @@ async function main() {
   {
     name: 'Gạo Lài hoa',
     description: `
-      <div class="content mx-auto w-full ">
-        <p class="text-gray-800 mb-4">
-          Gạo Lài hoa sở hữu hạt gạo trắng trong như hoa lài, mang đến hương thơm thanh khiết, dịu nhẹ khi nấu. Cơm có độ mềm xốp vừa phải, vị ngọt thanh tao. Đây là lựa chọn lý tưởng cho những ai yêu thích hương vị nhẹ nhàng, tinh tế trong từng hạt cơm.
+        <div class="content mx-auto w-full  font-fz-poppins">
+        <p class=" mb-4">
+          “Tựa như chính tên gọi của mình, gạo Lài Hoa mang theo sự tinh khiết và thanh tao - lựa chọn phù hợp cho những ai có khẩu vị thanh đạm”
         </p>
-        <div class="text-center  mb-4">
-          Hàm lượng dinh dưỡng trong 01kg
-        </div>
-        <div class="flex justify-between mb-4">
-          <table class="w-2/3 border-collapse border border">
-            <tbody>
-              <tr class="border border">
-                <td class="border border p-2">Đạm > 60g</td>
-              </tr>
-              <tr class="border border">
-                <td class="border border p-2">Tinh bột (Cacbonhydrat) > 720g</td>
-              </tr>
-              <tr class="border border">
-                <td class="border border p-2">Chất xơ (Fiber) > 3g</td>
-              </tr>
-            </tbody>
-          </table>
-          <table class="w-1/3 border-collapse border border">
-            <tbody>
-              <tr class="border border">
-                <td class="border border p-2">Chất béo (Lipid) < 2g</td>
-              </tr>
-              <tr class="border border">
-                <td class="border border p-2">Vitamin B1 < 100 μg</td>
-              </tr>
-              <tr class="border border">
-                <td class="border border p-2">Cholesterol = 0 g</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="text-center mb-4">
-          Chỉ tiêu chất lượng
-        </div>
-        <div class="flex justify-between">
-          <table class="w-2/3 border-collapse border border">
-            <tbody>
-              <tr class="border border">
-                <td class="border border p-2">Độ ẩm < 14%</td>
-              </tr>
-            </tbody>
-          </table>
-          <table class="w-1/3 border-collapse border border">
-            <tbody>
-              <tr class="border border">
-                <td class="border border p-2">Tấm < 5%</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <ul class="pl-5 space-y-2">
+          <li>
+            <p>
+              Gạo Lài Hoa – cái tên được lấy cảm hứng từ chính vẻ ngoài trắng trong như cánh hoa lài sớm, cùng hương thơm dịu dàng, thanh khiết đặc trưng. Giống gạo này thường được gieo trồng tại những vùng có khí hậu ôn hòa, nguồn nước sạch, ít chịu tác động của hóa chất, nhờ đó giữ trọn sự tinh khôi và tự nhiên trong từng hạt.
+            </p>
+          </li>
+          <li>
+            <p>
+              Tại Tư Trúc, gạo Lài Hoa được canh tác và sản xuất dưới quy trình nghiêm ngặt, đảm bảo hạt gạo thành phẩm thon dài, trắng mịn, đều tăm tắp, ít tấm, vẫn lưu giữ hương thơm nhẹ nhàng dù không qua xử lý tạo mùi.
+            </p>
+          </li>
+          <li>
+            <p>
+              Khi nấu chín, cơm tỏa hương thơm thoảng như hoa lài chớm nở, dịu nhẹ, không lấn át mùi vị món ăn. Cơm mềm xốp vừa phải, ngọt thanh và dễ ăn cả khi để nguội, phù hợp với những ăn thanh đạm hoặc dùng trong các bữa cơm chay thanh tịnh.
+            </p>
+          </li>
+        </ul>
       </div>
     `,
     imageFilename: 'lai_hoa.png',
@@ -190,60 +124,27 @@ async function main() {
   {
     name: 'Gạo Lài sữa',
     description: `
-      <div class="content mx-auto w-full ">
-        <p class="text-gray-800 mb-4">
-          Gạo Lài sữa có hạt gạo màu trắng sữa đặc trưng. Khi nấu, cơm dẻo mềm, có vị ngọt nhẹ và hương thơm thoang thoảng như sữa. Đây là giống gạo đặc biệt, không chỉ ngon miệng mà còn cung cấp nhiều dưỡng chất, phù hợp với cả người lớn và trẻ em.
+         <div class="content mx-auto w-full  font-fz-poppins">
+        <p class=" mb-4">
+          “Nhẹ nhàng như mùi sữa ấm, mang đến sự êm dịu trong từng bữa cơm - an lành cho mọi lứa tuổi, từ trẻ nhỏ đến người lớn tuổi”
         </p>
-        <div class="text-center  mb-4">
-          Hàm lượng dinh dưỡng trong 01kg
-        </div>
-        <div class="flex justify-between mb-4">
-          <table class="w-2/3 border-collapse border border">
-            <tbody>
-              <tr class="border border">
-                <td class="border border p-2">Đạm > 60g</td>
-              </tr>
-              <tr class="border border">
-                <td class="border border p-2">Tinh bột (Cacbonhydrat) > 720g</td>
-              </tr>
-              <tr class="border border">
-                <td class="border border p-2">Chất xơ (Fiber) > 3g</td>
-              </tr>
-            </tbody>
-          </table>
-          <table class="w-1/3 border-collapse border border">
-            <tbody>
-              <tr class="border border">
-                <td class="border border p-2">Chất béo (Lipid) < 2g</td>
-              </tr>
-              <tr class="border border">
-                <td class="border border p-2">Vitamin B1 < 100 μg</td>
-              </tr>
-              <tr class="border border">
-                <td class="border border p-2">Cholesterol = 0 g</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="text-center mb-4">
-          Chỉ tiêu chất lượng
-        </div>
-        <div class="flex justify-between">
-          <table class="w-2/3 border-collapse border border">
-            <tbody>
-              <tr class="border border">
-                <td class="border border p-2">Độ ẩm < 14%</td>
-              </tr>
-            </tbody>
-          </table>
-          <table class="w-1/3 border-collapse border border">
-            <tbody>
-              <tr class="border border">
-                <td class="border border p-2">Tấm < 5%</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <ul class="pl-5 space-y-2">
+          <li>
+            <p>
+              Gạo Lài Sữa là giống gạo đặc biệt với màu trắng đục và hương thơm nhẹ như sữa ấm. Được phát triển từ nguồn giống giàu dưỡng chất, Lài Sữa phù hợp canh tác ở vùng đất có độ mặn - ngọt luân phiên, giúp hạt gạo đạt độ dẻo mềm tự nhiên và vị ngọt thanh dễ chịu, đặc biệt dễ tiêu hóa.
+            </p>
+          </li>
+          <li>
+            <p>
+              Dựa trên hơn 30 năm kinh nghiệm, Tư Trúc đã xây dựng một quy trình trồng và xử lý riêng biệt dành cho dòng gạo này - loại gạo đặc thù với yêu cầu cao về độ tinh khiết và ổn định trong mỗi vụ mùa. Gạo thành phẩm thon dài, trắng đục, đều hạt, mang mùi phảng phất như sữa thơm.
+            </p>
+          </li>
+          <li>
+            <p>
+              Khi nấu chín, cơm dẻo mềm, thơm dịu như sữa thoảng, vị ngọt nhẹ nơi đầu lưỡi. Cơm không quá dính, không nát, giữ được độ mềm. Đây là lựa chọn lý tưởng cho trẻ em, người lớn tuổi, hoặc bất kỳ ai đang tìm kiếm một bữa cơm thanh đạm, nhẹ nhàng mà vẫn tròn vị.
+            </p>
+          </li>
+        </ul>
       </div>
     `,
     imageFilename: 'lai_sua.png',
@@ -256,60 +157,27 @@ async function main() {
   {
     name: 'Gạo Hương sen',
     description: `
-      <div class="content mx-auto w-full ">
-        <p class="text-gray-800 mb-4">
-          Gạo Hương sen mang đến trải nghiệm ẩm thực độc đáo với hương thơm thoang thoảng của lá sen tự nhiên. Hạt gạo thon dài, khi nấu cơm có độ dẻo vừa phải, không quá khô cũng không quá nát. Đây là sự lựa chọn thú vị để làm mới bữa ăn gia đình bạn, gợi nhớ đến những cánh đồng sen bát ngát.
+          <div class="content mx-auto w-full  font-fz-poppins">
+        <p class=" mb-4">
+          “Sự giao thoa giữa hạt gạo thượng hạng và hương đồng nội mộc mạc - mang lại trải nghiệm thanh nhẹ, gợi nhớ những mùa sen chớm nở”
         </p>
-        <div class="text-center  mb-4">
-          Hàm lượng dinh dưỡng trong 01kg
-        </div>
-        <div class="flex justify-between mb-4">
-          <table class="w-2/3 border-collapse border border">
-            <tbody>
-              <tr class="border border">
-                <td class="border border p-2">Đạm > 60g</td>
-              </tr>
-              <tr class="border border">
-                <td class="border border p-2">Tinh bột (Cacbonhydrat) > 720g</td>
-              </tr>
-              <tr class="border border">
-                <td class="border border p-2">Chất xơ (Fiber) > 3g</td>
-              </tr>
-            </tbody>
-          </table>
-          <table class="w-1/3 border-collapse border border">
-            <tbody>
-              <tr class="border border">
-                <td class="border border p-2">Chất béo (Lipid) < 2g</td>
-              </tr>
-              <tr class="border border">
-                <td class="border border p-2">Vitamin B1 < 100 μg</td>
-              </tr>
-              <tr class="border border">
-                <td class="border border p-2">Cholesterol = 0 g</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="text-center mb-4">
-          Chỉ tiêu chất lượng
-        </div>
-        <div class="flex justify-between">
-          <table class="w-2/3 border-collapse border border">
-            <tbody>
-              <tr class="border border">
-                <td class="border border p-2">Độ ẩm < 14%</td>
-              </tr>
-            </tbody>
-          </table>
-          <table class="w-1/3 border-collapse border border">
-            <tbody>
-              <tr class="border border">
-                <td class="border border p-2">Tấm < 5%</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <ul class="pl-5 space-y-2">
+          <li>
+            <p>
+              Gạo Hương Sen - giống gạo lấy cảm hứng từ hương thơm thanh nhẹ của lá sen non, gợi nhớ đến những buổi sớm thanh bình trên cánh đồng sen bát ngát với mùi hương tinh tế, không nồng, không gắt, không át vị món ăn - mang lại cảm giác dễ chịu.
+            </p>
+          </li>
+          <li>
+            <p>
+              Từ đồng ruộng đến nhà máy, mỗi hạt gạo Hương Sen là thành quả của đôi tay người nông dân cần mẫn và quy trình chế biến chuẩn mực, cho ra hạt gạo thon dài, trắng trong, đều hạt, mịn tay, giữ được hương thơm tự nhiên - yếu tố then chốt của các dòng gạo có mùi thơm đặc trưng.
+            </p>
+          </li>
+          <li>
+            <p>
+              Khi nấu chín, cơm dẻo mềm, không quá dính cũng không khô tơi, vị thanh nhẹ, dịu dàng. Hương sen thoảng qua lúc cơm chín lan tỏa vừa đủ để gợi cảm giác ngon miệng, phù hợp để làm mới bữa ăn hằng ngày, mang trọn vị mộc mạc từ đồng nội vào từng chén cơm.
+            </p>
+          </li>
+        </ul>
       </div>
     `,
     imageFilename: 'huong_sen.png',
@@ -324,23 +192,22 @@ async function main() {
     description: `
       <div class="content mx-auto w-full  font-fz-poppins">
         <p class=" mb-4">
-          Gạo ST25 là giống gạo đặc sản cao cấp của Việt Nam, với hương thơm tự nhiên, vị ngọt thanh và độ dẻo vừa phải, ST25 từng vinh dự đạt 
-          <strong>giải Nhất “Gạo ngon nhất thế giới” năm 2019</strong> tại Philippines.
+          “Hạt gạo ST25 - niềm tự hào của đất Việt mang trong mình độ dẻo thơm, ngọt hậu, giữ trọn vị ngon dù ăn nóng hay nguội, là lựa chọn lý tưởng cho mọi bữa cơm chất lượng”
         </p>
         <ul class="pl-5 space-y-2">
           <li>
             <p>
-              <strong>Loại gạo:</strong> Gạo trắng hạt dài, thon, trắng trong.
+              ST25 là giống gạo đặc sản cao cấp, được nhóm kỹ sư nông nghiệp Việt Nam lai tạo thành công. Vinh dự đoạt giải Nhất “Gạo ngon nhất thế giới” năm 2019 tại Hội nghị Thương mại Gạo Quốc tế (Philippines), ST25 gây ấn tượng mạnh nhờ chất lượng vượt trội và hương vị riêng biệt khó nhầm lẫn.
             </p>
           </li>
           <li>
             <p>
-              <strong>Hương vị:</strong> Thơm nhẹ mùi lá dứa đặc trưng, ngọt dịu sau khi nấu.
+              Tại Tư Trúc, gạo ST25 được giữ nguyên độ tinh khiết 100%, không pha trộn, không ướp hương nhân tạo, đảm bảo hạt gạo đến tay người dùng là nguyên bản nhất từ giống thuần chủng. Hạt gạo trắng, thon dài, đều tăm tắp, mang hương thơm dịu dàng như lá dứa non quyện nhẹ mùi cốm mới, dễ chịu và đầy cuốn hút.
             </p>
           </li>
           <li>
             <p>
-              <strong>Độ dẻo:</strong> Dẻo mềm nhưng không dính, cơm nguội vẫn ngon.
+              Khi nấu chín, cơm dẻo mềm vừa phải, giữ nguyên hình hạt, không nát, không dính, cho cảm giác dễ ăn và thanh nhã. Hương thơm lan tỏa nhẹ nhàng trong quá trình nấu, và vẫn phảng phất kể cả khi cơm nguội. Vị cơm ngọt hậu nhẹ nhàng, phù hợp với khẩu vị người Việt, và đủ tinh tế để chinh phục thị trường quốc tế.
             </p>
           </li>
         </ul>
